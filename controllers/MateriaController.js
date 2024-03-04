60411ff7f800647a2474ec9427af4de7d78a2013
FN2814 <138300002+FN2814@users.noreply.github.com>
2024-03-03 20:08:52 -0400

    Listado de materias

--- controllers/MateriaController.js
+++ controllers/MateriaController.js
@@ -1,7 +1,17 @@
 const mysql = require("mysql");
 const dbConfig = require("../dbConfig");
 
-
+ // Método para obtener todas las materias
+ static async listar(req, res) {
+  const sql = "SELECT * FROM materias";
+  try {
+    const materias = await dbQuery(sql);
+    res.json(materias);
+  } catch (error) {
+    console.error("Error al obtener materias:", error);
+    res.status(500).json({ error: "Error al obtener materias" });
+  }
+}
 
 // Función de utilidad para ejecutar consultas SQL
 function dbQuery(sql, params = []) {
